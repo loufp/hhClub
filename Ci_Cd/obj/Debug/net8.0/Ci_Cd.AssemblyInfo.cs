fda1bfd95ac8131e2318d5a0b84b82e677e071af
loufp <150954496+loufp@users.noreply.github.com>
2025-11-27 15:35:31 +0300

    nice

--- Ci_Cd/obj/Debug/net8.0/Ci_Cd.AssemblyInfo.cs
+++ Ci_Cd/obj/Debug/net8.0/Ci_Cd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ci_Cd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b611efa870de15053c6bebdecce74685c9abb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+083561afe8df268c75ce620e0c625fdf5b57eb6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ci_Cd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ci_Cd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
